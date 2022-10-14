9bcb6b9afaf4d3e5049a620001dfc2352bf81989
otisberri <otisberri@gmail.com>
2022-10-13 21:34:55 -0700

    Task four editted

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main is the entry point
+ *
+ * always return 0 to be sure of script success
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\Programming is like building a multilingual puzzle");
 	return (0);
 }
