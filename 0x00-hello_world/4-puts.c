1514d0d39488c3b1513712cd2b85014106e4a0c2
otisberri <otisberri@gmail.com>
2022-10-14 17:31:39 -0700

    Task 4 added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point of program 
+ *
+ * return 0 - Program success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
